e363a48284882e00b0b715bcfdfe7b23324ad879
chpag <115650339+chpag@users.noreply.github.com>
2023-09-18 18:08:40 +0200

    Update main.tf

--- vault/trust/main.tf
+++ vault/trust/main.tf
@@ -36,7 +36,7 @@ resource "vault_jwt_auth_backend_role" "tfc_role2" {
 
   bound_claims_type = "glob"
   bound_claims = {
-    sub = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}2:run_phase:*"
+    sub = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}3:run_phase:*"
   }
 
   user_claim = "terraform_full_workspace"

a373c2e00a247f3aebef5c600ebefca279b0fa1c
chpag <115650339+chpag@users.noreply.github.com>
2023-09-18 17:36:09 +0200

    Update main.tf

--- vault/trust/main.tf
+++ vault/trust/main.tf
@@ -27,6 +27,23 @@ resource "vault_jwt_auth_backend_role" "tfc_role" {
   token_ttl  = 1200
 }
 
+resource "vault_jwt_auth_backend_role" "tfc_role2" {
+  backend        = vault_jwt_auth_backend.tfc_jwt.path
+  role_name      = "tfc-role2"
+  token_policies = [vault_policy.tfc_policy.name]
+
+  bound_audiences = [var.tfc_vault_audience]
+
+  bound_claims_type = "glob"
+  bound_claims = {
+    sub = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}2:run_phase:*"
+  }
+
+  user_claim = "terraform_full_workspace"
+  role_type  = "jwt"
+  token_ttl  = 1200
+}
+
 resource "vault_policy" "tfc_policy" {
   name = "tfc-policy"
 

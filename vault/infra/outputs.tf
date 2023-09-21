8b7ad72674e096bcdf0fd337ae0c754f084a4b0d
chpag <115650339+chpag@users.noreply.github.com>
2023-09-21 10:04:10 +0200

    Update outputs.tf

--- vault/infra/outputs.tf
+++ vault/infra/outputs.tf
@@ -3,7 +3,7 @@ output "secret_name" {
   value       = vault_kv_secret_v2.example.name
 }
 
-output "secret_path" {
+output "secret_value" {
   description = "Path to the secret"
-  value       = vault_kv_secret_v2.example.path
+  value       = vault_kv_secret_v2.example.data_json
 }
